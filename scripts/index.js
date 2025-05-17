1de37c58812afd53c9f631bea00eafa4b6bf981d
gitalffa <fabricio.alffa@gmail.com>
2025-05-16 20:42:58 -0600

    corregido BEM

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,19 @@
 let page = document.querySelector(".page");
 let button_edit = page.querySelector(".button-edit");
-let fondo_negro = page.querySelector(".modo-edicion-none");
-let page_popup_edit = page.querySelector(".edicion__popup");
-let cerrar_edicion = page.querySelector(".edicion__close");
-let input_item = page.querySelectorAll("input__item");
+let fondo_negro = page.querySelector(".profile-editor__overlay--hidden");
+let page_popup_edit = page.querySelector(".profile-editor__popup");
+let cerrar_edicion = page.querySelector(".profile-editor__close-button");
+let input_item = page.querySelectorAll("profile-editor__input");
 const profileId = document.querySelector(".profile__id");
 
 // 1. Seleccionar elementos
-const form = document.querySelector(".edicion__form");
-const inputNombre = form.querySelector('.input__item[placeholder="name"]');
-const inputGrado = form.querySelector('.input__item[placeholder="Degree"]');
+const form = document.querySelector(".profile-editor__form");
+const inputNombre = form.querySelector(
+  '.profile-editor__input[placeholder="name"]'
+);
+const inputGrado = form.querySelector(
+  '.profile-editor__input[placeholder="Degree"]'
+);
 
 const profileName = document.querySelector(".profile__name");
 const profileDegree = document.querySelector(".profile__degree");
@@ -18,7 +22,9 @@ function manipulaFormEdit() {
   quitaNone();
   const nombre = profileId.querySelector(".profile__name").textContent;
   const grado = profileId.querySelector(".profile__degree").textContent;
-  const inputs = document.querySelectorAll(".edicion__form .input__item");
+  const inputs = document.querySelectorAll(
+    ".profile-editor__form .profile-editor__input"
+  );
   inputs[0].value = nombre;
   inputs[1].value = grado;
 }
@@ -39,13 +45,13 @@ form.addEventListener("submit", (event) => {
 });
 
 function quitaNone() {
-  fondo_negro.classList.remove("modo-edicion-none");
-  page_popup_edit.classList.remove("modo-edicion-none");
-  cerrar_edicion.classList.remove("modo-edicion-none");
+  fondo_negro.classList.remove("profile-editor__overlay--hidden");
+  page_popup_edit.classList.remove("profile-editor__overlay--hidden");
+  cerrar_edicion.classList.remove("profile-editor__overlay--hidden");
 }
 
 function poneNone() {
-  fondo_negro.classList.add("modo-edicion-none");
-  page_popup_edit.classList.add("modo-edicion-none");
-  cerrar_edicion.classList.add("modo-edicion-none");
+  fondo_negro.classList.add("profile-editor__overlay--hidden");
+  page_popup_edit.classList.add("profile-editor__overlay--hidden");
+  cerrar_edicion.classList.add("profile-editor__overlay--hidden");
 }
