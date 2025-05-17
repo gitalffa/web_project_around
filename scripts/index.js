4338f7f92092f2dfa2e66f6ae2bc32954829fb71
gitalffa <fabricio.alffa@gmail.com>
2025-05-17 00:14:37 -0600

    corregido BEM 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,14 @@
 let page = document.querySelector(".page");
-let button_edit = page.querySelector(".button-edit");
-let fondo_negro = page.querySelector(".profile-editor__overlay_hidden");
-let page_popup_edit = page.querySelector(".profile-editor__popup");
-let cerrar_edicion = page.querySelector(".profile-editor__close-button");
-let input_item = page.querySelectorAll("profile-editor__input");
+let buttonEdit = page.querySelector(".profile-button-edit");
+let popupEditorProfile = page.querySelector(".popup-editor-profile");
+let popupEditorProfileBase = page.querySelector(".popup-editor-profile__base");
+let popupEditorProfileCerrar = page.querySelector(
+  ".popup-editor-profile__close-button"
+);
+let popupEditorProfileInput = page.querySelectorAll(
+  ".popup-editor-profile__input"
+);
 const profileId = document.querySelector(".profile__id");
-
-// 1. Seleccionar elementos
-const form = document.querySelector(".profile-editor__form");
-const inputNombre = form.querySelector(
-  '.profile-editor__input[placeholder="name"]'
-);
-const inputGrado = form.querySelector(
-  '.profile-editor__input[placeholder="Degree"]'
-);
 
 const profileName = document.querySelector(".profile__name");
 const profileDegree = document.querySelector(".profile__degree");
@@ -23,18 +18,26 @@ function manipulaFormEdit() {
   const nombre = profileId.querySelector(".profile__name").textContent;
   const grado = profileId.querySelector(".profile__degree").textContent;
   const inputs = document.querySelectorAll(
-    ".profile-editor__form .profile-editor__input"
+    ".popup-editor-profile__form .popup-editor-profile__input"
   );
   inputs[0].value = nombre;
   inputs[1].value = grado;
 }
+
 function cerrarEdicion() {
   poneNone();
 }
 
-button_edit.addEventListener("click", manipulaFormEdit);
-cerrar_edicion.addEventListener("click", cerrarEdicion);
-// 2. Escuchar el submit del formulario
+buttonEdit.addEventListener("click", manipulaFormEdit);
+popupEditorProfileCerrar.addEventListener("click", cerrarEdicion);
+const form = document.querySelector(".popup-editor-profile__form");
+const inputNombre = form.querySelector(
+  '.popup-editor-profile__input[placeholder="name"]'
+);
+const inputGrado = form.querySelector(
+  '.popup-editor-profile__input[placeholder="Degree"]'
+);
+
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // prevenir recarga de página
 
@@ -45,13 +48,9 @@ form.addEventListener("submit", (event) => {
 });
 
 function quitaNone() {
-  fondo_negro.classList.remove("profile-editor__overlay_hidden");
-  page_popup_edit.classList.remove("profile-editor__overlay_hidden");
-  cerrar_edicion.classList.remove("profile-editor__overlay_hidden");
+  popupEditorProfile.classList.remove("popup-editor-profile_hidden");
 }
 
 function poneNone() {
-  fondo_negro.classList.add("profile-editor__overlay_hidden");
-  page_popup_edit.classList.add("profile-editor__overlay_hidden");
-  cerrar_edicion.classList.add("profile-editor__overlay_hidden");
+  popupEditorProfile.classList.add("popup-editor-profile_hidden");
 }
